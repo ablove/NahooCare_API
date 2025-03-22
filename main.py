34c964dbae5629d0fc14fc54e5fe809e207512db
Ashenafidejene <ashenafidejene75@gmail.com>
2025-03-22 18:14:18 +0300

    refactor main.py to include database and account controllers with API routing

--- main.py
+++ main.py
@@ -1,46 +1,6 @@
 from fastapi import FastAPI
-
+from controllers import database_controller, account_controller
 app = FastAPI()
 
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
-
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
-
-
-@app.get('/')
-def home():
-    print("this from home media")
-    return {"message": "this from home media"}  # Added return statement
-
-@app.post("/test/{id}")  # Missing curly braces {id}
-def test(id: int):
-    print(id, "this the id")
-    return {"id": id, "message": "this the id"}  # Added return statement
-
-'''
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
-
-uri = "mongodb+srv://ashenafidejene75:rItH4pBm3d0c2r3R@nahoocare.qgkx8.mongodb.net/?retryWrites=true&w=majority&appName=NahooCare"
-
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
-
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
-    
-    
-
-'''
+app.include_router(database_controller.router, prefix="/api", tags=["Database"])
+app.include_router(account_controller.router,prefix='/api/account', tags=['Account'])
