c435702c32fd7ae99bc38b3754e2716558777a3e
Ashenafidejene <ashenafidejene75@gmail.com>
2025-03-22 18:01:29 +0300

    adding  middleware for jwt authentication purpose

---
+++ middleware/auth.py
@@ -0,0 +1,15 @@
+from fastapi import Depends, HTTPException, Security
+from fastapi.security import OAuth2PasswordBearer
+import jwt
+from core.config import settings
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/account/login")
+
+def get_current_user(token: str = Security(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=["HS256"])
+        return payload["sub"]
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Token expired")
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Invalid token")
