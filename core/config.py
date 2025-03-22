92331a9680f1ea0f235f84732cf97afbfb481e7f
Ashenafidejene <ashenafidejene75@gmail.com>
2025-03-22 18:00:14 +0300

    adding config for connection setting to connect to databsae and security for authenticaction purpose

---
+++ core/config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings:
+    MONGO_CONNECTION_STRING: str = os.getenv("MONGO_CONNECTION_STRING")
+    MONGO_DB_NAME: str = os.getenv("MONGO_DB_NAME")
+    JWT_SECRET_KEY: str = os.getenv("JWT_SECRET_KEY")
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+
+settings = Settings()
---
+++ core/security.py
@@ -0,0 +1,21 @@
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+import jwt
+from core.config import settings
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# Hash a password
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+# Verify password
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+# Generate JWT Token
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (expires_delta if expires_delta else timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, settings.JWT_SECRET_KEY, algorithm="HS256")
