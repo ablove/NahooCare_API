b670ed53607cec900713102b4422f10013b2ef2e
Ashenafidejene <ashenafidejene75@gmail.com>
2025-04-09 03:04:04 +0300

    add symptom analysis controller and update config for OpenAI API key

--- core/config.py
+++ core/config.py
@@ -10,5 +10,5 @@ class Settings:
     MONGO_DB_NAME: str = os.getenv("MONGO_DB_NAME")
     JWT_SECRET_KEY: str = os.getenv("JWT_SECRET_KEY")
     ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
-
+    OPENAI_API_KEY : str = os.getenv("OPENAI_API_KEY")
 settings = Settings()
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from controllers import database_controller, account_controller,healthprofile_controller,healthcare_controller,rating_controller, admin_controller, first_aid_guide_controller, saved_search_controller
+from controllers import database_controller, account_controller,healthprofile_controller,healthcare_controller,rating_controller, admin_controller, first_aid_guide_controller, saved_search_controller,sympotm_analsis_controller
 
 app = FastAPI()
 
@@ -15,7 +15,7 @@ app.include_router(healthcare_controller.router, prefix="/api/healthcare", tags=
 app.include_router(rating_controller.router, prefix="/api/rating", tags=["Ratings"])
 app.include_router(admin_controller.router, prefix="/api/admin", tags=["Admin"])
 
-app.include_router(account_controller.router, prefix="/api/account", tags=["Account"])
 app.include_router(first_aid_guide_controller.router, prefix="/api/first-aid-guide", tags=["First Aid Guide"])
 app.include_router(saved_search_controller.router, prefix="/api/saved-searches", tags=["Saved Searches"])
+app.include_router(sympotm_analsis_controller.router,prefix='/api/SymptomAnalysis',tags=["Ai-Analysis"])
 
