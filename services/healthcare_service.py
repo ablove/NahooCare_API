f909888b30fae367a06079f892a8ebae978b176b
Ashenafidejene <ashenafidejene75@gmail.com>
2025-04-09 13:47:56 +0300

    fix: update healthcare center creation logic to use name for uniqueness and generate center_id

--- services/healthcare_service.py
+++ services/healthcare_service.py
@@ -1,3 +1,4 @@
+import uuid
 from fastapi import HTTPException
 from db.mongodb import database
 from models.healthcare_model import HealthcareCenter
@@ -15,18 +16,20 @@ async def create_healthcare_center(center: HealthcareCenterCreate):
 
     
     try:
-        existing_center = await collection.find_one({"center_id": center.center_id})
+        existing_center = await collection.find_one({"name": center.name})
         if existing_center:
-            raise HTTPException(status_code=400, detail="Healthcare center ID already exists")
+            raise HTTPException(status_code=400, detail="Healthcare name already exists")
 
         center_data = center.dict()
-        center_data["location"] = {"type": "Point", "coordinates": [center.longitude, center.latitude]}  # Geospatial field
-
+        
+        center_data["location"] = {"type": "Point", "coordinates": [center.longitude, center.latitude]} 
+        # Geospatial field
+        center_data["center_id"] = str(uuid.uuid4())
         result = await collection.insert_one(center_data)
         if not result.inserted_id:
             raise HTTPException(status_code=500, detail="Failed to create healthcare center")
 
-        return str(result.inserted_id)
+        return True 
 
     except Exception as e:
         logging.error(f"Error creating healthcare center: {e}")
