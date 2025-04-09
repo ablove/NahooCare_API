e96861ef29139ddc0baec63bbce6f78d7a4c12b4
Ashenafidejene <ashenafidejene75@gmail.com>
2025-04-09 13:47:07 +0300

    fix: remove center_id from HealthcareCenterCreate and ensure available_Time field is consistently defined

--- schemas/healthcare_schema.py
+++ schemas/healthcare_schema.py
@@ -3,14 +3,13 @@ from typing import List, Optional
 
 
 class HealthcareCenterCreate(BaseModel):
-    center_id: str = Field(..., example="3001")
     name: str = Field(..., example="Yekatit 12 General Hospital")
     address: str = Field(..., example="Sdsst Kilo, Addis Ababa")
     latitude: float = Field(..., example=9.0456)
     longitude: float = Field(..., example=38.7612)
     specialists: List[str] = Field(..., example=["Cardiology", "Neurology"])
     contact_number: str = Field(..., example="+251111234567")
-
+    available_Time : List[str] = Field(..., example=["it works every day 8:00 am - 10:pm "])
 
 class HealthcareSearch(BaseModel):
     specialty: str = Field(..., example="Cardiology")
@@ -24,3 +23,4 @@ class HealthcareCenterUpdate(BaseModel):
     longitude: Optional[float] = Field(None, example=38.7623)
     specialists: Optional[List[str]] = Field(None, example=["Pediatrics", "Cardiology"])
     contact_number: Optional[str] = Field(None, example="+251987654321")
+    available_Time : List[str] = Field(..., example=["it works every day 8:00 am - 10:pm "])
